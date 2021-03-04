e049ab23496b12a0396c5503b83a69d33c45fccd
kassandra <kassandra.dominguez@sjsu.edu>
2021-03-03 16:45:14 -0800

    5th commit for investment.py

--- investment.py
+++ investment.py
@@ -1,5 +1,5 @@
 def calculate_apr(principal, interest_rate, years): #Takes in principle, interest_rate and years and calculates value of investment after X years'''
-    if ((principal or interest_rate or years) <= 0):
+    if ( (principal<=0) or (interest_rate<=0) or (years<= 0) ):
         return False
     else: 
         total=float( (principal*(1+interest_rate)**years) )
